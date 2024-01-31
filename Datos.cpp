bca5e272067c8f1a1b7e4c3a283e73b5fcc9f34c
Simón Navarro <angel.navarro9286@alumnos.udg.mx>
2024-01-31 08:52:59 -0600

    Agregué el trabajo de programació de Datos

---
+++ Datos.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+NOMBRE: Angel Simón Navarro Martín
+FECHA: 29/01/2024
+PROGRAMA: Datos
+CENTRO UNIVERSITARIO DE LOS ALTOS / UNIVERSIDAD DE GUADALAJARA
+INGENIERÍA EN COMPUTACIÓN / 2DO SEMESTRE
+PROFESOR: Sergio Franco Casillas
+DESCRIPCIÓN: Mostrar Nombre, Edad, Lugar de residencia, Fecha, Carrera y Semestre
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Nombre: Angel Simon Navarro Martin " << endl;
+    cout << "Edad: 18 years " << endl;
+    cout << "Lugar de residencia: Capilla de Guadalupe" << endl;
+    cout << "Fecha: 29 de Enero de 2024  " << endl;
+    cout << "Carrera: Computacion" << endl;
+    cout << "Semestres: 2do" << endl;
+    return 0;
+}
